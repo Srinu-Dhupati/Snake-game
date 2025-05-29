366512d4c76ae4391588990bee3f852632a135b3
Srinu-Dhupati <141562512+Srinu-Dhupati@users.noreply.github.com>
2025-05-29 20:01:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,79 @@
-from turtle import  Screen
-import time
-from snake import Snake
-from Food import Food
-from Score import Score
-# creaking border
+"""Primary application entrypoint.
+"""
+import locale
+import logging
+import os
+import sys
+import warnings
+from typing import List, Optional
 
-snake = Snake()
-food = Food()
-score = Score()
-# tim = Turtle()
-screen = Screen()
-screen.bgcolor("black")
-screen.setup(width=600, height=600)
-screen.tracer(0)
+from pip._internal.cli.autocompletion import autocomplete
+from pip._internal.cli.main_parser import parse_command
+from pip._internal.commands import create_command
+from pip._internal.exceptions import PipError
+from pip._internal.utils import deprecation
 
-
-# creating snake shape
-# all_positions = [(0, 0), (-20, 0), (-40, 0)]
-# all_snakes = []
-
-game_over = True
+logger = logging.getLogger(__name__)
 
 
+# Do not import and use main() directly! Using it directly is actively
+# discouraged by pip's maintainers. The name, location and behavior of
+# this function is subject to change, so calling it directly is not
+# portable across different pip versions.
 
-user_bet = screen.textinput(title="chose your level", prompt="easy or medium or hard").lower()
+# In addition, running pip in-process is unsupported and unsafe. This is
+# elaborated in detail at
+# https://pip.pypa.io/en/stable/user_guide/#using-pip-from-your-program.
+# That document also provides suggestions that should work for nearly
+# all users that are considering importing and using main() directly.
 
-screen.listen()
-screen.onkey(snake.move_up, "Up")
-screen.onkey(snake.move_down, "Down")
-screen.onkey(snake.move_right, "Left")
-screen.onkey(snake.move_left, "Right")
+# However, we know that certain users will still want to invoke pip
+# in-process. If you understand and accept the implications of using pip
+# in an unsupported manner, the best approach is to use runpy to avoid
+# depending on the exact location of this entry point.
 
-screen.setup(width=500, height=500)
-# snake movement
-while game_over:
-    screen.update()
-    if user_bet == "easy":
-        time.sleep(0.4)
-    elif user_bet == "medium":
-        time.sleep(0.2)
-    elif user_bet == "hard":
-        time.sleep(0.1)
-    snake.move()
-
-    # detecting food collision and score board
-    if snake.head.distance(food)<15:
-        score.increase_score()
-        food.refresh()
-        snake.extend()
-
-    # detecting collision with walls
-    if (snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290
-            or snake.head.ycor() < -290):
-        game_over = False
-        score.game_over()
-
-    # detecting collision with its own tail
-    for segment in snake.all_snakes[1:]:
-        if snake.head.distance(segment) < 15:
-            game_over = False
-            score.game_over()
+# The following example shows how to use runpy to invoke pip in that
+# case:
+#
+#     sys.argv = ["pip", your, args, here]
+#     runpy.run_module("pip", run_name="__main__")
+#
+# Note that this will exit the process after running, unlike a direct
+# call to main. As it is not safe to do any processing after calling
+# main, this should not be an issue in practice.
 
 
+def main(args: Optional[List[str]] = None) -> int:
+    if args is None:
+        args = sys.argv[1:]
 
+    # Suppress the pkg_resources deprecation warning
+    # Note - we use a module of .*pkg_resources to cover
+    # the normal case (pip._vendor.pkg_resources) and the
+    # devendored case (a bare pkg_resources)
+    warnings.filterwarnings(
+        action="ignore", category=DeprecationWarning, module=".*pkg_resources"
+    )
 
+    # Configure our deprecation warnings to be sent through loggers
+    deprecation.install_warning_logger()
 
-screen.exitonclick()
+    autocomplete()
+
+    try:
+        cmd_name, cmd_args = parse_command(args)
+    except PipError as exc:
+        sys.stderr.write(f"ERROR: {exc}")
+        sys.stderr.write(os.linesep)
+        sys.exit(1)
+
+    # Needed for locale.getpreferredencoding(False) to work
+    # in pip._internal.utils.encoding.auto_decode
+    try:
+        locale.setlocale(locale.LC_ALL, "")
+    except locale.Error as e:
+        # setlocale can apparently crash if locale are uninitialized
+        logger.debug("Ignoring error %s when setting locale", e)
+    command = create_command(cmd_name, isolated=("--isolated" in cmd_args))
+
+    return command.main(cmd_args)
